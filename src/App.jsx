ebe0ca1c33d8104f2ce34d6b31984090cbb33684
srdjan-gr <srdjan.gr@gmail.com>
2023-06-05 11:11:14 +0200

    pages add. routs setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
+import MainLayout from './Layouts/MainLayout'
+import Home from './Pages/Home'
+import Beatz from './Pages/Beatz'
+import Organic from './Pages/Organic'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>} >
+      <Route  index element={<Home/>} />
+      <Route path='/organic'  element={<Organic/>}/>
+      <Route path='/beatz' element={<Beatz/>} />
+    </Route>
+  )
+)
+
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/Pages/Beatz.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Beatz = () => {
+  return (
+    <div>Beatz</div>
+  )
+}
+
+export default Beatz
